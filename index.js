a1234dc5103995669d386b788e0ae35393cefaac
Larissa Miranda <larissamiranda@MacBook-Air-de-Larissa.local>
2021-12-07 19:45:30 -0300

    criacao de resolver

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const {ApolloServer, gql} = require ('apollo-server')
 
+const usuarios = [{
+    id: 1,
+    nome: 'Lucas de Abreu',
+    email: 'lucasabreu@gmail.com',
+    idade: 29
+}, {
+    id: 2,
+    nome: 'Lucia Lima',
+    email: 'lucia.lima@gmail.com',
+    idade: 42
+}, {
+    id: 3,
+    nome: 'Maria Silva',
+    email: 'ms@gmail.com',
+    idade: 18
+}]
+
 const typeDefs = gql`
     scalar Date
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
 
     type Usuario {
         id: ID!
@@ -17,10 +41,25 @@ const typeDefs = gql`
         ola: String
         horaAtual: Date
         usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+        numerosMegaSena: [Int!]!
+        usuarios: [Usuario]
+        usuario(id: ID): Usuario
     }
 `
 
 const resolvers = {
+    Produto: {
+        precoComDesconto(produto) {
+            if(produto.desconto) {
+                return produto.preco * (1 - produto.desconto)
+            } else {
+                return produto.preco
+            }
+
+        }
+    },
+
     Usuario: {
         salario(usuario) {
             return usuario.salario_real
@@ -44,6 +83,27 @@ const resolvers = {
                 salario_real: 1234.56,
                 vip: true
             }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: 'pera',
+                preco: 5.50,
+                desconto: 0.15
+
+            }
+        },
+        numerosMegaSena() {
+            const crescente = (a, b) => a - b
+            return Array(6).fill(0)
+                .map(n => parseInt(Math.random() * 60 + 1))
+                .sort(crescente)
+        },
+        usuarios() {
+            return usuarios
+        },
+        usuario(_, args){
+            const selecionados = usuarios.filter(u => u.id ==  args.id)
+            return selecionados ? selecionados[0] : null
         }
     }
 }
