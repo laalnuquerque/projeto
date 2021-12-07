3ebd54a2c7c3fc4bae455d2a52e0fe3578c4f372
Larissa Miranda <larissamiranda@MacBook-Air-de-Larissa.local>
2021-12-07 18:19:07 -0300

    configuração servidor Apollo

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
 const {ApolloServer, gql} = require ('apollo-server')
 
-const typeDefs = ''
+const typeDefs = gql`
+    scalar Date
 
-const resolvers = {}
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean    
+    }
+
+    # Pontos de entrada da API
+    type Query {
+        ola: String
+        horaAtual: Date
+        usuarioLogado: Usuario
+    }
+`
+
+const resolvers = {
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+
+    },
+
+    Query: {
+        ola() {
+            return 'Bom Dia'
+        },
+        horaAtual() {
+            return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Ana del rey',
+                email: 'anadelrey@gmail.com',
+                idade: 23,
+                salario_real: 1234.56,
+                vip: true
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em  ${url}`)
+})
